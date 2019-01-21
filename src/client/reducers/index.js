6e5e8062c1c3bd88f72550bb72ba46d4aabb384f
JJ Jacobs <jrjacobs24@gmail.com>
2019-01-21 15:32:27 -0500

    Setup boilerplate for using react-router
    * Will be using `connected-react-router` to utilize react-router via
      a redux workflow

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 import notes from './notesReducer';
 import noteDialog from './noteDialogReducer';
 
-export default combineReducers({
-  notes,
-  noteDialog,
-});
+export default history =>  combineReducers(
+  {
+    router: connectRouter(history),
+    notes,
+    noteDialog,
+  }
+);
+
+/** Selectors */
+export const getRouterLocation = state => state.router.location;
--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,16 +1,19 @@
 import createSagaMiddleware from 'redux-saga';
 import { applyMiddleware, compose, createStore } from 'redux';
 import createRootReducer from 'reducers';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
 
 const sagaMiddleware = createSagaMiddleware();
+export const history = createBrowserHistory();
 
 /** @see https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
-  createRootReducer,
+  createRootReducer(history),
   {},
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware))
 );
 
 export const runSaga = sagaMiddleware.run;
