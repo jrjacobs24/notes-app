75e817eb85b0e19220c36f962225a3c46584c530
JJ Jacobs <jrjacobs24@gmail.com>
2019-01-21 11:47:37 -0500

    Setup webpack config and root react starting point
    * Installed Google's Material React lib for UI
    * Moved webpack and babel to dev deps
    * Added html template for webpack build

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDir = 'dist';
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/client/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, outputDir),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src/client/'), 'node_modules'],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin([outputDir]),
+    new HTMLWebpackPlugin({
+      title: 'Notes For the Taking',
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+};
